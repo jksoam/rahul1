6a9e14ef3569b46ff59ea7632c8820d0bdf86ccf
jksoam <jksoam007@gmail.com>
2025-08-31 01:11:30 +0530

    Update main.tf

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_subnet" "snet" {
     for_each = var.subnets
     name     = each.value.name
-    resource_group_name = each.value.resource_group.name
-    virtual_network_name = each.value.virtual_network.name
+    resource_group_name = each.value.resource_group_name
+    virtual_network_name = each.value.virtual_network_name
     address_prefixes = each.value.address_prefixes
 
     # Optional properties can be added here if needed
