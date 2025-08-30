bc454c33e27a2d294994784465318368fa95f7e3
jksoam <jksoam007@gmail.com>
2025-08-31 01:02:05 +0530

    Update main.tf

--- vnet/main.tf
+++ vnet/main.tf
@@ -3,6 +3,6 @@ resource "azurerm_virtual_network" "vnet" {
     name     = each.value.name
     location = each.value.location
     address_space = each.value.address_space
-    resource_group_name = azurerm_resource_group.rg[each.value.resource_group].name
+    resource_group_name = each.value.resource_group_name
   
 }
