a086a14cdde53b48495b60b3cad4762356eeee11
jksoam <jksoam007@gmail.com>
2025-08-31 00:37:54 +0530

    Update provider.tf

---
+++ env/prod/provider.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "3ace2f6c-7fad-4649-99ca-c60df54b9873"
+}
